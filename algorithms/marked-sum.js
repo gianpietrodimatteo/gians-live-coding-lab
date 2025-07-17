09f52f1eea63355b2fbef6410078566582c077c6
gianpietrodimatteo <gianpietro.dimatteo@gmail.com>
2025-07-16 21:25:37 -0300

    Factoring indexes, still not working

--- algorithms/marked-sum.js
+++ algorithms/marked-sum.js
@@ -26,18 +26,25 @@ const testCases = {
 }
 
 // we know arr.length > 0           OK  
-const findMinimal = (arr) => {
+const findMinimal = (arr, markedIndexes) => {
+    // TODO potential problem here with arr[0]
     let minimal = arr[0];
-    arr.forEach(element => {
-        if (element < minimal)
+    arr.forEach((element, index) => {
+        if (element < minimal && !markedIndexes[index])
             minimal = element;
     });
     return minimal;
 }
 
-// we know it's there               OK
-const findMinimalIndex = (arr, min) => {
-    return arr.findIndex(element => element === min);
+// we know it's there               
+const findMinimalIndex = (arr, min, markedIndexes) => {
+    // Find the min factoring the marked indexes
+    // return arr.findIndex(element => element === min);
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === min && !markedIndexes[i])
+            return i;
+    }
 }
 
 // whatever if we mark unnecessarely, shouldn't be a problem    OK
@@ -52,13 +59,20 @@ const doShit = (values) => {
 
     const markedIndexes = {};
 
-    const minimal = findMinimal(values);
-    const minimalIndex = findMinimalIndex(values, minimal);
-    console.log(minimalIndex);
-    markAdjascentIndexes(minimalIndex, markedIndexes)
-    console.log(markedIndexes);
+    // const minimal = findMinimal(values, markedIndexes);
+    // const minimalIndex = findMinimalIndex(values, minimal);
+    // markAdjascentIndexes(minimalIndex, markedIndexes)
+    // console.log({ minimal, minimalIndex, markedIndexes });
 
-    finalSum = minimal;
+    // finalSum = minimal;
+
+    for (let i = 0; i < 3; i++) {
+        const minimal = findMinimal(values, markedIndexes);
+        const minimalIndex = findMinimalIndex(values, minimal, markedIndexes);
+        markAdjascentIndexes(minimalIndex, markedIndexes)
+        console.log({ minimal, minimalIndex, markedIndexes });
+        finalSum += minimal;
+    }
 
     return finalSum;
 }
